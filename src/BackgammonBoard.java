2a28e689e7149c2946c3fc10bf04133f6606b967
ayushijain1198 <ayushi11jain98@gmail.com>
2023-11-04 18:09:55 +0000

    for loop commit

--- src/BackgammonBoard.java
+++ src/BackgammonBoard.java
@@ -1,19 +1,35 @@
 public class BackgammonBoard {
     public static void display() {
-        System.out.println("24 23 22 21 20 19 18 |BAR| 17 16 15 14 13 12 11");
+        System.out.println("  24 23 22 21 20 19 18 |BAR| 17 16 15 14 13 12 11");
         System.out.println("+-------------------+---+-------------------+");
-        System.out.println("|  X  X  X  X  X  X  |   |  O  O  O  O  O  O  |");
-        System.out.println("|  X  X  X  X  X  X  |   |  O  O  O  O  O  O  |");
-        System.out.println("|  X  X  X  X  X  X  |   |  O  O  O  O  O  O  |");
-        System.out.println("|  X  X  X  X  X  X  |   |  O  O  O  O  O  O  |");
-        System.out.println("|  X  X  X  X  X  X  |   |  O  O  O  O  O  O  |");
-        System.out.println("|                   |BAR|                     |");
-        System.out.println("|  O  O  O  O  O  O  |   |  X  X  X  X  X  X  |");
-        System.out.println("|  O  O  O  O  O  O  |   |  X  X  X  X  X  X  |");
-        System.out.println("|  O  O  O  O  O  O  |   |  X  X  X  X  X  X  |");
-        System.out.println("|  O  O  O  O  O  O  |   |  X  X  X  X  X  X  |");
-        System.out.println("|  O  O  O  O  O  O  |   |  X  X  X  X  X  X  |");
+
+        for (int row = 0; row < 5; row++) {
+            System.out.print("| ");
+            for (int i = 0; i < 6; i++) {
+                System.out.print("X  ");
+            }
+            System.out.print("|   | ");
+            for (int i = 0; i < 6; i++) {
+                System.out.print("O  ");
+            }
+            System.out.println("|");
+        }
+
+        System.out.println("|                   |BAR|                   |");
+
+        for (int row = 0; row < 5; row++) {
+            System.out.print("| ");
+            for (int i = 0; i < 6; i++) {
+                System.out.print("O  ");
+            }
+            System.out.print("|   | ");
+            for (int i = 0; i < 6; i++) {
+                System.out.print("X  ");
+            }
+            System.out.println("|");
+        }
+
         System.out.println("+-------------------+---+-------------------+");
-        System.out.println("  1  2  3  4  5  6  7 |BAR| 8  9 10 11 12 13 14");
+        System.out.println("1  2  3  4  5  6  7 |BAR| 8  9 10 11 12 13 14");
     }
 }
